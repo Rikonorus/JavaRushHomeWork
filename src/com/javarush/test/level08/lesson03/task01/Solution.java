9efaf8d907ecb933268389bb47a37ddc581a16ff
djavad199450@gmail.com <Kahramanov631994>
2016-09-09 09:00:23 +0300

    finished 8.4

---
+++ src/com/javarush/test/level08/lesson03/task01/Solution.java
@@ -0,0 +1,36 @@
+package com.javarush.test.level08.lesson03.task01;
+
+/* HashSet из растений
+Создать коллекцию HashSet с типом элементов String.
+Добавить в неё 10 строк: арбуз, банан, вишня, груша, дыня, ежевика, жень-шень, земляника, ирис, картофель.
+Вывести содержимое коллекции на экран, каждый элемент с новой строки.
+Посмотреть, как изменился порядок добавленных элементов.
+*/
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Solution
+{
+    public static void main(String[] args) throws Exception
+    {
+        Set<String> hashSet = new HashSet<>();
+        hashSet.add("арбуз");
+        hashSet.add("банан");
+        hashSet.add("вишня");
+        hashSet.add("груша");
+        hashSet.add("дыня");
+        hashSet.add("ежевика");
+        hashSet.add("жень-шень");
+        hashSet.add("земляника");
+        hashSet.add("ирис");
+        hashSet.add("картофель");
+        for (String setItem :
+                hashSet)
+        {
+            System.out.println(setItem);
+        }
+        //напишите тут ваш код
+
+    }
+}
