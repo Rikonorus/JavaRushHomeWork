b05e6054a8326be5847151f15b62dfe461db81d4
djavad199450@gmail.com <Kahramanov631994>
2016-09-14 10:41:22 +0300

    finish lev 09 less 05 finished

--- src/com/javarush/test/level09/lesson11/home04/Solution.java
+++ src/com/javarush/test/level09/lesson11/home04/Solution.java
@@ -2,6 +2,7 @@ package com.javarush.test.level09.lesson11.home04;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
@@ -15,10 +16,11 @@ import java.util.Locale;
 public class Solution {
 
     public static void main(String[] args) throws Exception {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String oldDate = reader.readLine();
-        Date dt = new Date(oldDate);
-
         //напишите тут ваш код
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+//        Date dt = new Date("08/18/2013");
+        Date dt = new Date(reader.readLine());
+        SimpleDateFormat df = new SimpleDateFormat("MMM dd, YYYY", Locale.ENGLISH);
+        System.out.println(df.format(dt).toUpperCase());
     }
 }
