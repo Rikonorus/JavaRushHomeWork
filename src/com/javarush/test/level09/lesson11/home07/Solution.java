d48beadfa361199845790af074a19b0e0bc8b23c
rikonorus <Kahramanov631994>
2016-09-15 13:18:43 +0300

    try to pass - not passed

--- src/com/javarush/test/level09/lesson11/home07/Solution.java
+++ src/com/javarush/test/level09/lesson11/home07/Solution.java
@@ -6,22 +6,27 @@ package com.javarush.test.level09.lesson11.home07;
 
 public class Solution
 {
-  /*  public int A = 5;
-    public int B = 2 * A;
+    public static int A = 5;
+    public static int B = 2 * A;
     public int C = A * B;
-    public int D = A * B;
+    public static int D = A * B;
 
     public static void main(String[] args)
     {
         Solution room = new Solution();
         room.A = 5;
+        Solution room2 = new Solution();
+        room2.A = 6;
 
         Solution.D = 5;
+        System.out.println(room.A);
+        System.out.println(room2.A);
+        System.out.println(Solution.A);
     }
 
     public int getA()
     {
         return A;
     }
-*/
+
 }
