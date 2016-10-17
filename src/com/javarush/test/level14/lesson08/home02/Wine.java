4be08d11103faab0d1e59ae9a425b32103535822
djavad199450@gmail.com <Kahramanov631994>
2016-10-17 23:44:55 +0300

    14.8 processed

---
+++ src/com/javarush/test/level14/lesson08/home02/Wine.java
@@ -0,0 +1,11 @@
+package com.javarush.test.level14.lesson08.home02;
+
+/**
+ * Created by Rustam on 17.10.2016.
+ */
+public class Wine extends Drink
+{
+    public String getHolidayName(){
+        return "День рождения";
+    }
+}
