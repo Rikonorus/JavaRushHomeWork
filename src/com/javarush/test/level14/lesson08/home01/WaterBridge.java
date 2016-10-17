fc10bb0a3ed74f0cc8caf804fe73ff164de72ff6
djavad199450@gmail.com <Kahramanov631994>
2016-10-17 08:41:15 +0300

    14.8 processed

---
+++ src/com/javarush/test/level14/lesson08/home01/WaterBridge.java
@@ -0,0 +1,11 @@
+package com.javarush.test.level14.lesson08.home01;
+
+/**
+ * Created by Rustam on 17.10.2016.
+ */
+public class WaterBridge implements Bridge
+{
+    public int getCarsCount(){
+        return 5;
+    }
+}
