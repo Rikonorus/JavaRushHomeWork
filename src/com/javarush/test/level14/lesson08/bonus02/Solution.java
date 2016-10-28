7230231c8b7a48a6feb798d014a96a567e87b5f0
rikonorus <Kahramanov631994>
2016-10-28 19:31:37 +0300

    14.8 processed

---
+++ src/com/javarush/test/level14/lesson08/bonus02/Solution.java
@@ -0,0 +1,27 @@
+package com.javarush.test.level14.lesson08.bonus02;
+
+/* НОД
+Наибольший общий делитель (НОД).
+Ввести с клавиатуры 2 целых положительных числа.
+Вывести в консоль наибольший общий делитель.
+*/
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Solution
+{
+    public static void main(String[] args) throws Exception
+    {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        Integer a = Integer.parseInt(reader.readLine());
+        Integer b = Integer.parseInt(reader.readLine());
+        System.out.println(NOD(a, b));
+    }
+
+    private static int NOD(Integer a, Integer b)
+    {
+
+        return 0;
+    }
+}
